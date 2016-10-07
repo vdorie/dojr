71f583fe5978d7383cfafa47acc110181b4b6d4d
Vincent Dorie <vjd4@nyu.edu>
2016-10-07 13:59:27 -0400

    color fix

--- common/src/knitr.R
+++ common/src/knitr.R
@@ -1,5 +1,6 @@
-colFmt <- function(x, color) {
+colFmt <- function(color, x) {
   outputFormat = knitr::opts_knit$get("rmarkdown.pandoc.to")
+  if (is.null(outputFormat)) outputFormat <- rmarkdown::all_output_formats(knitr::current_input())[1L]
   switch(outputFormat, 
          latex = paste0("\\textcolor{", color, "}{", x, "}"),
          html  = paste0("<font color='", color, "'>", x, "</font>"),

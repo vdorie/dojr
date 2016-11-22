b4fd67f0594b9773beae1c9f8a43410f2e78ee24
Vincent Dorie <vdorie@gmail.com>
2016-11-22 18:51:21 -0500

    disclosure scenarios

--- common/src/knitr.R
+++ common/src/knitr.R
@@ -40,62 +40,62 @@ truncateString <- function(x, length)
 
 rmdFormat <- function(x, ...) { UseMethod("rmdFormat", x) }
 
-rmdFormat.data.frame <- function(x, maxRows = 25L, ...)
+rmdFormat.data.frame <- function(x, colWidths = NULL, maxRows = 25L, ...)
 {
-  stringResult <- NULL
-  stringConnection <- textConnection("stringResult", "w", local = TRUE)
-  sink(stringConnection)
-  
   x.char <- sapply(x, function(x.i) format(x.i, ...))
-  lengths <- sapply(seq_along(x), function(i) max(nchar(x.char[1L, i]), nchar(names(x))[i]))
+  if (nrow(x) > maxRows)
+    x.char[maxRows,] <- "..."
+  printLimit <- if (maxRows < nrow(x)) maxRowsL else nrow(x)
   
-  for (i in seq_along(x)) {
-    if (i > 1L) cat(" | ")
-    cat(sprintf("%-*s", lengths[i], names(x)[i]))
-  }
-  cat("\n")
-  for (i in seq_along(x)) {
-    if (i > 1L) cat(" | ")
-    for (j in seq_len(lengths[i])) cat("-")
-  }
-  cat("\n")
-  printLimit <- if (nrow(x) > maxRows) maxRows - 1L else nrow(x)
-  for (j in seq_len(printLimit)) {
-    for (i in seq_along(x)) {
-      if (i > 1L) cat(" | ")
-      cat(sprintf("%-*s", lengths[i], x.char[j,i]))
-    }
-    cat("\n") 
-  }
-  if (printLimit != nrow(x)) {
-    for (i in seq_along(x)) {
-      if (i > 1L) cat(" | ")
-      numDots <- min(lengths[i], 3L)
-      for (j in seq_len(numDots)) cat(".")
-      if (numDots < lengths[i]) for (j in seq.int(numDots + 1L, lengths[i])) cat(" ")
-    }
-    cat("\n")
-  }
-  sink()
-  close(stringConnection)
-
-  stringResult
+  rmdFormat.matrix(x.char[seq.int(1L, printLimit),], colWidths, ...)
 }
 
-rmdFormat.matrix <- function(x, ...)
+rmdFormat.matrix <- function(x, colWidths = NULL, ...)
 {
+  catRow <- function(x, maxLengths, printLengths) {
+    x.str <- lapply(seq_along(x), function(col) strwrap(x[col], maxLengths[col] + 1L))
+    numLines <- max(sapply(x.str, length))
+    
+    for (i in seq_len(numLines)) {
+      cat("| ")
+      for (col in seq_along(x)) {
+        if (col > 1L) cat(" | ")
+        x.j <- if (length(x.str[[col]]) > i) paste0(x.str[[col]][i], "\\") else { if (length(x.str[[col]]) < i) "" else x.str[[col]][i] }
+        cat(sprintf("%-*s", printLengths[col], x.j))
+      }
+      cat(" |\n")
+    }
+  }
+  catHeaderSeparator <- function(printLengths) {
+    cat("+=")
+    for (col in seq_along(printLengths)) {
+      if (col > 1L) cat("=+=")
+      for (j in seq_len(printLengths[col])) cat("=")
+    }
+    cat("=+\n")
+  }
+  catRowSeparator <- function(printLengths) {
+    cat("+-")
+    for (col in seq_along(printLengths)) {
+      if (col > 1L) cat("-+-")
+      for (j in seq_len(printLengths[col])) cat("-")
+    }
+    cat("-+\n")
+  }
+  
+  
   stringResult <- NULL
   stringConnection <- textConnection("stringResult", "w", local = TRUE)
   sink(stringConnection)
   
   x.char  <- sapply(seq_len(ncol(x)), function(col) format(x[,col], ...))
-  lengths <- sapply(seq_len(ncol(x)), function(col) max(nchar(x.char[1L, col]), nchar(colnames(x))[col]))
+  maxLengths <- sapply(seq_len(ncol(x)), function(col) max(nchar(x.char[1L, col]), nchar(colnames(x))[col]))
   colnames(x.char) <- colnames(x)
   rownames(x.char) <- NULL
   
   if (!is.null(rownames(x))) {
     x.char <- cbind(rownames(x), x.char)
-    lengths <- c(max(nchar(rownames(x))), lengths)
+    maxLengths <- c(max(nchar(rownames(x))), maxLengths)
   }
   
   hasExtraHeaders <- if (!is.null(dimnames(x))) any(!is.null(names(dimnames(x)))) else FALSE
@@ -109,66 +109,70 @@ rmdFormat.matrix <- function(x, ...)
       } else {
         extraHeaders[1L] <- paste0(extraHeaders[1L], "\\")
       }
-      lengths[1L] <- max(nchar(extraHeaders[1L]), lengths[1L])
+      maxLengths[1L] <- max(nchar(extraHeaders[1L]), maxLengths[1L])
     }
     if (ncol(x) > 1L && !is.null(extraHeaders[2L])) {
       extraHeaders[2L] <- paste0(extraHeaders[2L], "\\")
-      lengths[2L] <- max(nchar(extraHeaders[2L]), lengths[2L])
+      maxLengths[2L] <- max(nchar(extraHeaders[2L]), maxLengths[2L])
     }
+  }
+    
+  if (!is.null(colWidths)) {
+    if (length(colWidths) == 1L) colWidths <- rep(colWidths, ncol(x.char))
+    
+    if (length(colWidths) == ncol(x)) {
+      replaceRange <- seq.int(length(maxLengths) - ncol(x) + 1L, length(maxLengths))
+      printLengths <- ifelse(colWidths < maxLengths[replaceRange], colWidths + 1L, maxLengths[replaceRange])
+      if (length(printLengths) < length(maxLengths)) printLengths <- c(maxLengths[1L], printLengths)
+      maxLengths[replaceRange] <- pmin(maxLengths[replaceRange], colWidths)
+      
+      if (hasExtraHeaders) browser()
+    } else {
+      browser()
+    }
+    
+    
+  } else {
+    printLengths <- maxLengths
   }
   
   cat("+-")
   for (col in seq_len(ncol(x.char))) {
     if (col > 1L) cat("-+-")
-    for (row in seq_len(lengths[col])) cat("-")
+    for (row in seq_len(printLengths[col])) cat("-")
   }
   cat("-+\n")
   if (hasExtraHeaders) {
     cat("| ")
-    if (!is.null(extraHeaders[1L]) && is.null(rownames(x))) cat(sprintf("%-*s", lengths[1L], extraHeaders[1L]))
-    else for (j in seq_len(lengths[1L])) cat(" ")
+    if (!is.null(extraHeaders[1L]) && is.null(rownames(x))) cat(sprintf("%-*s", printLengths[1L], extraHeaders[1L]))
+    else for (j in seq_len(printLengths[1L])) cat(" ")
     if (ncol(x.char) > 1L) {
       cat(" | ")
-      if (!is.null(extraHeaders[2L])) cat(sprintf("%-*s", lengths[2L], extraHeaders[2L]))
+      if (!is.null(extraHeaders[2L])) cat(sprintf("%-*s", printLengths[2L], extraHeaders[2L]))
       if (ncol(x) > 2L) for (i in seq.int(3L, ncol(x.char))) {
         cat(" | ")
-        for (j in seq_len(lengths[i])) cat(" ")
+        for (j in seq_len(printLengths[i])) cat(" ")
       }
     }
     cat(" |\n")
   }
-  cat("| ")
-  for (col in seq_len(ncol(x.char))) {
-    if (col > 1L) cat(" | ")
-    cat(sprintf("%-*s", lengths[col], colnames(x.char)[col]))
-  }
-  cat(" |\n")
-  cat("+=")
-  for (col in seq_len(ncol(x.char))) {
-    if (col > 1L) cat("=+=")
-    for (j in seq_len(lengths[col])) cat("=")
-  }
-  cat("=+\n")
+  
+  ## can't name dimensions with a '.', so replace those that are interstital
+  headerNames <- gsub("([^.])\\.([^.])", "\\1 \\2", colnames(x.char), perl = TRUE)
+  
+  catRow(headerNames, maxLengths, printLengths)
+  
+  catHeaderSeparator(printLengths)
+  
   for (row in seq_len(nrow(x.char))) {
-    cat("| ")
-    for (col in seq_len(ncol(x.char))) {
-      if (col > 1L) cat(" | ")
-      cat(sprintf("%-*s", lengths[col], x.char[row,col]))
-    }
-    cat(" |\n")
-    if (row != nrow(x.char)) {
-      cat("+-")
-      for (col in seq_len(ncol(x.char))) {
-        if (col > 1L) cat("-+-")
-        for (j in seq_len(lengths[col])) cat("-")
-      }
-      cat("-+\n")
-    }
+    catRow(x.char[row,], maxLengths, printLengths)
+    if (row != nrow(x.char))
+      catRowSeparator(printLengths)
   }
   cat("+-")
   for (col in seq_len(ncol(x.char))) {
     if (col > 1L) cat("-+-")
-    for (j in seq_len(lengths[col])) cat("-")
+    for (j in seq_len(printLengths[col])) cat("-")
   }
   cat("-+\n")
   

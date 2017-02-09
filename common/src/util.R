d15000b45b0046240208d38b33f305ceff18155a
Vincent Dorie <vdorie@gmail.com>
2017-02-09 14:06:45 -0500

    added times to dispositions

--- common/src/util.R
+++ common/src/util.R
@@ -1,1 +1,21 @@
 "%not_in%" <- function(x, table) match(x, table, nomatch = 0L) <= 0L
+
+remapFactor <- function(x, groups, name, droplevels = TRUE) {
+  if (!is.list(groups)) groups <- list(groups)
+  if (length(groups) != length(name)) stop("length of groups != length of name")
+  for (i in seq_along(groups)) {
+    if (length(groups[[i]]) > 1L) x[x %in% groups[[i]][-1L]] <- groups[[i]][1L]
+    levels(x)[levels(x) == groups[[i]][1L]] <- name[i]
+  }
+  if (droplevels) x <- base::droplevels(x)
+  x
+}
+
+## evaluates the expression 'e' by binding its first argument as 'x'
+evalx <- function(x, e) {
+  mc <- match.call()
+  callingEnv <- parent.frame()
+  evalEnv <- new.env(parent = callingEnv)
+  evalEnv$x <- x
+  eval(mc$e, evalEnv)
+}

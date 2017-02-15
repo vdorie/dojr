aeebedd54a0c904cd956bd4ea69247d858c39229
Vincent Dorie <vdorie@gmail.com>
2017-02-15 14:26:17 -0500

    diversity in arrest severity

--- anonymization/src/suppression.R
+++ anonymization/src/suppression.R
@@ -29,7 +29,7 @@ enforceLDiversity <- function(x, across, by, inQuasiIdentifiers, forSensitiveVal
     
     x.sub <- x[subRows, union(inQuasiIdentifiers, suppressing)]
     if (length(forSensitiveValues) == 1L) {
-      x.sub[["sensitive_key"]] <- as.factor(x[subRows, forSensitiveValues])
+      x.sub[,"sensitive_key"] <- as.factor(x[subRows, forSensitiveValues])
     } else {
       x.sub[,"sensitive_key"] <- as.factor(apply(sapply(x[subRows, forSensitiveValues], as.character), 1L, function(row) paste0(row, collapse = ":")))
     }
@@ -43,6 +43,46 @@ enforceLDiversity <- function(x, across, by, inQuasiIdentifiers, forSensitiveVal
     numSuppressed[i,"l %"] <- 100 * numSuppressed[i,"l #"] / nrow(x.sub)
     
     x[subRows, suppressing][dropRows] <- NA
+  }
+  
+  list(suppressed = x[,suppressing], numSuppressed = numSuppressed)
+}
+
+## the lesser category should come first in mappedFrom, mappedTo
+enforceOrderedDiversity <- function(x, across, by, inQuasiIdentifiers, forSensitiveValue, mappedFrom, mappedTo, suppressing)
+{
+  numSuppressed <- matrix(NA_real_, length(by), 2L, dimnames = list(as.character(by), c("o #", "o %")))
+  
+  for (i in seq_along(by)) {
+    subRows <- x[,across] == by[i]
+    
+    x.sub <- x[subRows, union(inQuasiIdentifiers, c(suppressing, forSensitiveValue))]
+    
+    x.sub[,"sensitive_key"] <- remapFactor(x.sub[,forSensitiveValue], mappedFrom, mappedTo)
+    
+    summaryTable <- table(x.sub[,c(inQuasiIdentifiers, "sensitive_key")])
+    atRiskTable <- apply(summaryTable, seq_len(length(dim(summaryTable)) - 1L), function(x) {
+      ifelse(x[mappedTo[2L]] > 0L & x[mappedTo[1L]] == 0L, TRUE, FALSE)
+    })
+    
+    tableDims <- dim(atRiskTable)
+    dimNames <- dimnames(atRiskTable)
+   
+    indexExpression <- quote(match(x.sub[,inQuasiIdentifiers[1L]], dimNames[[1L]]))
+    for (j in seq.int(2L, length(dim(atRiskTable)))) {
+      temp <- quote(a + b)
+      temp[[2L]] <- indexExpression
+      temp[[3L]] <- substitute((match(x.sub[,inQuasiIdentifiers[j]], dimNames[[j]]) - 1L) * prod(head(tableDims, j - 1L)), list2env(list(j = j)))
+      indexExpression <- temp
+    }
+    
+    suppressRows <- atRiskTable[eval(indexExpression)]
+    suppressRows[is.na(suppressRows)] <- FALSE
+    
+    numSuppressed[i,"o #"] <- sum(suppressRows)
+    numSuppressed[i,"o %"] <- 100 * numSuppressed[i,"o #"] / nrow(x.sub)
+    
+    x[subRows, suppressing][suppressRows] <- NA
   }
   
   list(suppressed = x[,suppressing], numSuppressed = numSuppressed)
